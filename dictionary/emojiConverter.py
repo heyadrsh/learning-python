bc5e23a081973f0a6958531a1a1f92915540e9a3
heyadrsh <heyadrsh@gmail.com>
2023-01-12 12:00:00 +0530

    Added dictionary learning examples and exercises

--- dictionary/emojiConverter.py
+++ dictionary/emojiConverter.py
@@ -1,1 +1,15 @@
- 
+# Emoji converter using dictionary
+message = input("> ")
+words = message.split(" ")
+
+emojis = {
+    ":)": "😊",
+    ":(": "😢",
+    ":D": "😃"
+}
+
+output = ""
+for word in words:
+    output += emojis.get(word, word) + " "
+
+print(output)
