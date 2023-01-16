60e02f2627c28b1bca86b3eb14ab05182577b163
heyadrsh <heyadrsh@gmail.com>
2023-01-16 19:30:15 +0530

    Added module organization with packages - Created ecommerce package with proper structure

---
+++ modules/example/app.py
@@ -0,0 +1,8 @@
+from modules.example.ecommerce.sales import calc_shipping, calc_tax
+import modules.example.ecommerce.sales as sales
+
+sales.calc_tax()
+sales.calc_shipping()
+
+calc_shipping()
+calc_tax()
